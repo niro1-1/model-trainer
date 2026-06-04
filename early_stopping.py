7d7cab28ae06f1cce261cc925fa8763b411102c3
niro1-1 <niro1-1@walla.co.il>
2026-06-04 16:54:43 +0300

    Add early stopping with patience

--- early_stopping.py
+++ early_stopping.py
@@ -1,20 +1,18 @@
 class EarlyStopping:
-    def __init__(self, patience=5, verbose=0):
+    def __init__(self, patience=5):
         self.patience = patience
-        self.verbose = verbose
-        self.best_score = None
-        self.early_stop = False
         self.counter = 0
+        self.best_score = None
 
-    def __call__(self, score):
+    def step(self, score):
         if self.best_score is None:
             self.best_score = score
-        elif score < self.best_score:
+            return False
+        if score < self.best_score:
             self.counter += 1
             if self.counter >= self.patience:
-                self.early_stop = True
-                if self.verbose:
-                    print("Early stopping triggered")
+                return True
         else:
             self.best_score = score
             self.counter = 0
+        return False
